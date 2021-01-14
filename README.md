eb930c174ab6914e642cd8080755d9e094ba6f66
KolesnikAntony <50579516+KolesnikAntony@users.noreply.github.com>
2021-01-14 05:22:55 +0200

    Change ReadMe

--- README.md
+++ README.md
@@ -1,29 +1,4 @@
-# home-forms
-Обозначил по списку:
-### pages
-1. [profile](http://localhost:3000/profile.html) Профиль done
-2. [deal](http://localhost:3000/deal.html) Сделка done
-3. [p-wallet](http://localhost:3000/p-wallet.html) Кошелек done
-4. [mortgage-main](http://localhost:3000/index.html)  После перехода с главной done
-5. [mortgage open](http://localhost:3000/mortgage-open.html) Развернутые подразделы (mortagege-open.html) done
-6. [mortgage open borrower](http://localhost:3000/mortgage-open-borrower.html) Развернутые подразделы с заемщиком (mortgage-open-borrower.html) done 
-7. [mortgage success](http://localhost:3000/mortgage-success.html) ЛК-ипотека, одобрено/отказано (mortagege-success.html) done
-8. [mortgage-upload](http://localhost:3000/mortgage-upload.html) ЛК-ипотека, загрузка объекта и оценка (mortgage-upload.html) done
-9. [mortgage-insurance](http://localhost:3000/mortgage-insurance.html) ЛК-ипотека, Страховка ЛК (mortgage-insurance.html)
-10. [mortgage-check](http://localhost:3000/mortgage-check.html) ЛК-ипотека, Просто забивать из ЛК (check.html)
 
-
-### Netlify
-1. [profile](https://home-forms.netlify.app/profile.html) Профиль done
-2. [deal](https://home-forms.netlify.app/deal.html) Сделка done
-3. [p-wallet](https://home-forms.netlify.app/p-wallet.html) Кошелек done
-4. [mortgage-main](https://home-forms.netlify.app/index.html)  После перехода с главной done
-5. [mortgage open](https://home-forms.netlify.app/mortgage-open.html) Развернутые подразделы (mortagege-open.html) done
-6. [mortgage open borrower](https://home-forms.netlify.app/mortgage-open-borrower.html) Развернутые подразделы с заемщиком (mortgage-open-borrower.html) done 
-7. [mortgage success](https://home-forms.netlify.app/mortgage-success.html) ЛК-ипотека, одобрено/отказано (mortagege-success.html) done
-8. [mortgage-upload](https://home-forms.netlify.app/mortgage-upload.html) ЛК-ипотека, загрузка объекта и оценка (mortgage-upload.html) done
-9. [mortgage-insurance](https://home-forms.netlify.app/mortgage-insurance.html) ЛК-ипотека, Страховка ЛК (mortgage-insurance.html)
-10. [mortgage-check](https://home-forms.netlify.app/mortgage-check.html) ЛК-ипотека, Просто забивать из ЛК (check.html)
 
 ## Gulp boilerplate
 > All you need for static website coding. Includes live reload, SASS,  autoprefixer, EJS/PUG template engines, images reducing, JS/SASS linters, FTP deployment, JS bundling with browserify, ES2015+ ready, JS/CSS minification.*
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,16 +1,84 @@
+var calcBtn = document.getElementById('calc');
+var firstIn = document.getElementById('first-in');
+var secondIn = document.getElementById('price-car');
+var periodIn = document.getElementById('period');
+firstIn.addEventListener('change', function() {
+    calculate();
+});
+secondIn.addEventListener('change', function() {
+    calculate();
+});
+periodIn.addEventListener('change', function() {
+    calculate();
+});
 
-let calcBtn = document.getElementById('calc');
+function calculate() {
+    var firstPayIn = document.getElementById('first-in').value;
+    var week = document.getElementById('first-out');
+    var monthly = document.getElementById('month-pay');
+    var originalPrice = document.getElementById('price-car').value;
+    var period = document.getElementById('period').value;
+    var changePrice = originalPrice - firstPayIn;
+    var payMonth;
 
-calcBtn.onclick = calculate;
+    var result = function result() {
+        var dirtySum;
 
-let calculate =() => {
-    let firstPayIn = document.getElementById('first-in').value;
-    let originalPrice = document.getElementById('price-car').value;
-    let period =document.getElementById('period').value;
-    console.log( firstPayIn);
-    console.log( originalPrice);
-    console.log( period);
+        if (period == 1) {
+            dirtySum = changePrice * 1.0698;
+        } else if (period == 2) {
+            dirtySum = changePrice * 1.1398;
+        } else if (period == 3) {
+            dirtySum = changePrice * 1.2098;
+        } else if (period == 4) {
+            dirtySum = changePrice * 1.2565;
+        } else if (period == 5) {
+            dirtySum = changePrice * 1.3031;
+        } else if (period == 6) {
+            dirtySum = changePrice * 1.3498;
+        } else if (period == 7) {
+            dirtySum = changePrice * 1.3931;
+        } else if (period == 8) {
+            dirtySum = changePrice * 1.4364;
+        } else if (period == 9) {
+            dirtySum = changePrice * 1.4798;
+        } else if (period == 10) {
+            dirtySum = changePrice * 1.5198;
+        } else if (period == 11) {
+            dirtySum = changePrice * 1.5598;
+        } else if (period == 12) {
+            dirtySum = changePrice * 1.5998;
+        }
 
+        payMonth = dirtySum / period;
+    };
+
+    result();
+    monthly.value = Math.floor(payMonth);
+    week.value = Math.floor(payMonth / 4);
 }
 
-
+$(document).ready(function() {
+    //E-mail Ajax Send
+    $(".calc__form").submit(function() {
+        if (originalPrice < 6000) {
+            alert('Минимальная цена 6000zl');
+            return false
+        }
+        //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "/mail.php",
+            //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+});
